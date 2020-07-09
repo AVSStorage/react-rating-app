137c2f64434acb5a0fc91c24e77853af0a47fc58
Alice <alise-fox@inbox.ru>
2020-07-09 16:19:30 +0300

    Updates

---
+++ precache-manifest.303674cf383892bf3d77b36d9dcd1249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed4dba3364942e447be46ba058734b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9502d01826d5824602",
+    "url": "/static/css/main.924aff4d.chunk.css"
+  },
+  {
+    "revision": "8b1f19ca92f12eb8afed",
+    "url": "/static/js/2.df6fbbdf.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/static/js/2.df6fbbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9502d01826d5824602",
+    "url": "/static/js/main.7115c99b.chunk.js"
+  },
+  {
+    "revision": "6fb65f13b774faadd314",
+    "url": "/static/js/runtime-main.f30e385d.js"
+  }
+]);
